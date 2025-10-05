bc4b8dbbc76e947571aad873e99d46ce1c1c7fd3
emexSW <jonsons.hub@gmail.com>
2025-10-05 19:25:50 +0200

    [DOCCR] version 1

---
+++ build/doccr.d
@@ -0,0 +1,50 @@
+build/./doccr.o: doccr.cpp doccr.hpp ../../shared/utils/console.hpp \
+ ../../shared/types.h ../../shared/data_struct/ring_buffer.hpp \
+ ../../shared/ui/draw/draw.h ../../shared/ui/graphic_types.h \
+ ../../shared/std/string.h ../../shared/std/args.h ../../shared/std/std.h \
+ ../../shared/std/allocator.hpp ../../shared/data_struct/data_struct.hpp \
+ ../../shared/data_struct/array.hpp ../../shared/syscalls/syscalls.h \
+ ../../shared/keyboard_input.h ../../shared/mouse_input.h \
+ ../../shared/net/network_types.h ../../shared/files/fs.h \
+ ../../shared/data_struct/chunked_list.hpp \
+ ../../shared/data_struct/chunked_list.h \
+ ../../shared/data_struct/double_linked_list.hpp \
+ ../../shared/data_struct/double_linked_list.h \
+ ../../shared/data_struct/indexmap.hpp \
+ ../../shared/data_struct/linked_list.hpp \
+ ../../shared/data_struct/linked_list.h \
+ ../../shared/data_struct/queue.hpp ../../shared/std/memory.h \
+ ../../shared/data_struct/queue.h ../../shared/std/memory.h \
+ ../../shared/math/math.h ../../shared/math/rng.h \
+ ../../shared/input_keycodes.h
+doccr.hpp:
+../../shared/utils/console.hpp:
+../../shared/types.h:
+../../shared/data_struct/ring_buffer.hpp:
+../../shared/ui/draw/draw.h:
+../../shared/ui/graphic_types.h:
+../../shared/std/string.h:
+../../shared/std/args.h:
+../../shared/std/std.h:
+../../shared/std/allocator.hpp:
+../../shared/data_struct/data_struct.hpp:
+../../shared/data_struct/array.hpp:
+../../shared/syscalls/syscalls.h:
+../../shared/keyboard_input.h:
+../../shared/mouse_input.h:
+../../shared/net/network_types.h:
+../../shared/files/fs.h:
+../../shared/data_struct/chunked_list.hpp:
+../../shared/data_struct/chunked_list.h:
+../../shared/data_struct/double_linked_list.hpp:
+../../shared/data_struct/double_linked_list.h:
+../../shared/data_struct/indexmap.hpp:
+../../shared/data_struct/linked_list.hpp:
+../../shared/data_struct/linked_list.h:
+../../shared/data_struct/queue.hpp:
+../../shared/std/memory.h:
+../../shared/data_struct/queue.h:
+../../shared/std/memory.h:
+../../shared/math/math.h:
+../../shared/math/rng.h:
+../../shared/input_keycodes.h:
